2c2fdf0120a0fd3043aa75219d612bb1a56d0a95
Marcio Mendes <mmendes@autoadgroup.net>
2026-07-30 14:39:17 -0400

    refactor: Remove dropdown component and associated files

--- src/app/components/fields-types/dropdown/dropdown.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dropdown',
-  imports: [],
-  templateUrl: './dropdown.html',
-  styleUrl: './dropdown.css',
-})
-export class Dropdown {}
